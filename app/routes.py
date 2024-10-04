a7bffa785c1c216f3ff0bb05188906e57d428810
Björn Thiberg <bthiberg@kth.se>
2024-10-04 11:56:45 +0200

    remove frontend

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,3 @@ api_bp = Blueprint('api', __name__)
 @api_bp.route('/api', methods=['GET'])
 def get_data():
     return jsonify({'message': 'Hello from Flask API!'})
-
-@api_bp.route('/')
-def index():
-    return render_template('index.html')
